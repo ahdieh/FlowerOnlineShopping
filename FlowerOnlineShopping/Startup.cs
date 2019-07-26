ccbd4155e59246c63b67bef051af8e3f8190b266
ahdieh emadi <ahdieh_e@yahoo.com>
2019-07-26 00:49:43 -0400

    First commit

---
+++ FlowerOnlineShopping/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlowerOnlineShopping.Startup))]
+namespace FlowerOnlineShopping
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
